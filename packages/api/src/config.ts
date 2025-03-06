e7bfbddc044cf3664710085197b0a65720bd66ba
Michi Palazzo <palazzomichi@gmail.com>
2025-03-06 23:17:29 +0100

    Add config (#7)
    * add config
    
    * implement config
    
    * update loadConfigFromEnv type

---
+++ packages/api/src/config.ts
@@ -0,0 +1,27 @@
+import { Type, type Static } from '@sinclair/typebox';
+import { Value, AssertError } from '@sinclair/typebox/value';
+
+const Config = Type.Object({
+  host: Type.String(),
+  port: Type.Number({ minimum: 0, maximum: 65535 }),
+});
+
+export type Config = Static<typeof Config>;
+
+export function loadConfigFromEnv():
+  | { config: Config; error: undefined }
+  | { config: undefined; error: string } {
+  const config = {
+    host: process.env.HOST ?? 'localhost',
+    port: process.env.PORT ?? '0',
+  };
+
+  try {
+    return { config: Value.Parse(Config, config), error: undefined };
+  } catch (error) {
+    if (error instanceof AssertError && error.error) {
+      return { config: undefined, error: `${error.error.path}: ${error.error.message}` };
+    }
+    throw error;
+  }
+}

20f986d85dab1abf5c083f6fe7c792f54290ca9f
Michi Palazzo <palazzomichi@gmail.com>
2025-03-10 14:51:48 +0100

    improve migration

--- packages/database/migrations/2025-03-07.ts
+++ packages/database/migrations/2025-03-07.ts
@@ -3,10 +3,11 @@ import type { Kysely } from 'kysely';
 export async function up(db: Kysely<unknown>): Promise<void> {
   await db.schema
     .createTable('users')
+    .ifNotExists()
     .addColumn('id', 'serial', (col) => col.primaryKey())
     .execute();
 }
 
 export async function down(db: Kysely<unknown>): Promise<void> {
-  await db.schema.dropTable('users').execute();
+  await db.schema.dropTable('users').ifExists().execute();
 }

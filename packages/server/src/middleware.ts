fc3fc42dd0ee2669c5e02e94ad60861220541580
Michi Palazzo <palazzomichi@gmail.com>
2025-03-27 00:34:12 +0100

    Reuse Middleware type

--- packages/server/src/middleware.ts
+++ packages/server/src/middleware.ts
@@ -2,7 +2,7 @@ import type { Route } from './types';
 
 export type Middleware = (route: Route) => Route;
 
-export function use(...middlewares: ((route: Route) => Route)[]) {
+export function use(...middlewares: Middleware[]) {
   return (target: object | undefined, name: string | symbol): void => {
     if (!target) throw new Error('Middleware decorator can only be used on methods');
     let value = Reflect.get(target, name) as Route;

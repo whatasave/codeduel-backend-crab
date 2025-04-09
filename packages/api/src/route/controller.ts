480a6b2166646951f05751743403a8190160cc72
Michi Palazzo <palazzomichi@gmail.com>
2025-04-09 23:48:00 +0200

    fix vibe coding

--- packages/api/src/route/controller.ts
+++ packages/api/src/route/controller.ts
@@ -6,32 +6,35 @@ import { UserController } from './user/controller';
 import { UserRepository } from './user/repository';
 import { UserService } from './user/service';
 import type { Database } from '@codeduel-backend-crab/database';
+import { ChallengeRepository } from './challenge/repository';
+import { ChallengeService } from './challenge/service';
+import { ChallengeController } from './challenge/controller';
 
 export class RootController {
   private readonly userRepository: UserRepository;
-  private readonly challengeRepository: UserRepository;
+  private readonly challengeRepository: ChallengeRepository;
 
   private readonly healthService: HealthService;
   private readonly userService: UserService;
-  private readonly challengeService: UserService;
+  private readonly challengeService: ChallengeService;
 
   private readonly redocController: RedocController;
   private readonly healthController: HealthController;
   private readonly userController: UserController;
-  private readonly challengeController: UserController;
+  private readonly challengeController: ChallengeController;
 
   constructor(database: Database) {
     this.userRepository = new UserRepository(database);
-    this.challengeRepository = new UserRepository(database);
+    this.challengeRepository = new ChallengeRepository(database);
 
     this.healthService = new HealthService();
     this.userService = new UserService(this.userRepository);
-    this.challengeService = new UserService(this.challengeRepository);
+    this.challengeService = new ChallengeService(this.challengeRepository);
 
     this.redocController = new RedocController();
     this.healthController = new HealthController(this.healthService);
     this.userController = new UserController(this.userService);
-    this.challengeController = new UserController(this.challengeService);
+    this.challengeController = new ChallengeController(this.challengeService);
   }
 
   setup(group: RouterGroup): void {

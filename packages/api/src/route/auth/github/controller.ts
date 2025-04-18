de392a6e7e2ba1415ab6836f94c5eeb44db89e90
Kaj Rusilowski <xedom08@gmail.com>
2025-04-18 23:34:17 +0200

    update github controller

--- packages/api/src/route/auth/github/controller.ts
+++ packages/api/src/route/auth/github/controller.ts
@@ -1,6 +1,7 @@
 import {
   internalServerError,
   temporaryRedirect,
+  permanentRedirect,
   type RouterGroup,
 } from '@codeduel-backend-crab/server';
 import { validated } from '@codeduel-backend-crab/server/validation';
@@ -49,39 +50,41 @@ export class GithubController {
           state: Type.String(),
         },
       },
-      response: {},
+      response: {
+        308: Type.Object({}),
+      },
     },
     handler: async ({ query }) => {
       const { code, state } = query;
 
-      if (!code || !state) {
-        return internalServerError({ message: 'Missing code or state' });
-      }
+      if (!code || !state) return internalServerError({ message: 'Missing code or state' });
       const cookieState = this.githubService.getState(state);
 
-      if (state !== cookieState) {
-        return internalServerError({ message: 'Invalid state' });
-      }
+      if (state !== cookieState) return internalServerError({ message: 'Invalid state' });
 
       const token = await this.githubService.accessToken(code, state);
-      if (!token) {
-        return internalServerError({ message: 'Failed to get access token' });
-      }
+      if (!token) return internalServerError({ message: 'Failed to get access token' });
 
       const user = await this.githubService.userData(token.accessToken);
-      if (!user) {
-        return internalServerError({ message: 'Failed to get user data' });
-      }
+      if (!user) return internalServerError({ message: 'Failed to get user data' });
 
       const authUser =
         (await this.githubService.userByProvider(user.id)) ??
         (await this.githubService.create(user));
 
+      if (!authUser) return internalServerError({ message: 'Failed to get user' });
+
       const tokens = await this.githubService.tokens(authUser);
 
-      return temporaryRedirect({
+      if (!tokens) return internalServerError({ message: 'Failed to get tokens' });
+      const cookieAccessToken = this.githubService.createCookie(tokens.accessToken);
+      const cookieRefreshToken = this.githubService.createCookie(tokens.refreshToken);
+      const cookieExpiresIn = this.githubService.createCookie(tokens.expiresIn.toString());
+
+      return permanentRedirect({
         headers: {
-          Location: this.githubService.getRedirectUrl(authUser),
+          Location: 'http://localhost',
+          'Set-Cookie': [cookieAccessToken, cookieRefreshToken, cookieExpiresIn].join(','),
         },
       });
     },

c364f1de2d4800bf4bd42708e56126d8d530914f
Kaj Rusilowski <xedom08@gmail.com>
2025-04-19 15:15:58 +0200

    add migration for auth

---
+++ packages/database/migrations/20250419_auth.ts
@@ -0,0 +1,29 @@
+import { sql, type Kysely } from 'kysely';
+import { updateTimestampTrigger } from './20250406_update_timestamp';
+
+export const TABLE_NAME = 'auth';
+
+const [createTrigger, dropTrigger] = updateTimestampTrigger(TABLE_NAME);
+
+export async function up(db: Kysely<unknown>): Promise<void> {
+  await db.schema
+    .createTable(TABLE_NAME)
+    .ifNotExists()
+    .addColumn('user_id', 'serial', (col) => col.notNull())
+    .addColumn('provider', 'varchar(255)', (col) => col.notNull())
+    .addColumn('provider_id', 'serial', (col) => col.notNull().unique())
+    .addColumn('created_at', 'timestamptz', (col) =>
+      col.notNull().defaultTo(sql`CURRENT_TIMESTAMP`)
+    )
+    .addColumn('updated_at', 'timestamptz', (col) =>
+      col.notNull().defaultTo(sql`CURRENT_TIMESTAMP`)
+    )
+    .execute();
+
+  await createTrigger(db);
+}
+
+export async function down(db: Kysely<unknown>): Promise<void> {
+  await db.schema.dropTable(TABLE_NAME).ifExists().execute();
+  await dropTrigger(db);
+}

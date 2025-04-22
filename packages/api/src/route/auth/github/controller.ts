e77088d7d2df839984feda51c860b7fa6785b241
Kaj Rusilowski <xedom08@gmail.com>
2025-04-22 20:49:22 +0200

    refactor GithubController to integrate AuthService and improve error handling

--- packages/api/src/route/auth/github/controller.ts
+++ packages/api/src/route/auth/github/controller.ts
@@ -8,9 +8,13 @@ import { validated } from '@codeduel-backend-crab/server/validation';
 import type { GithubService } from './service';
 import { randomUUIDv7 } from 'bun';
 import { Type } from '@sinclair/typebox';
+import type { AuthService } from '../service';
 
 export class GithubController {
-  constructor(private readonly githubService: GithubService) {}
+  constructor(
+    private readonly githubService: GithubService,
+    private readonly authService: AuthService
+  ) {}
 
   setup(group: RouterGroup): void {
     group.route(this.login);
@@ -31,7 +35,9 @@ export class GithubController {
       const cookie = this.githubService.createCookie(state);
       const redirectUrl = this.githubService.getAuthorizationUrl(state);
 
-      console.log('Redirecting to GitHub for authorization:', redirectUrl);
+      console.log('Generated state: \n\t', state);
+      console.log('Generated cookie: \n\t', cookie);
+      console.log('Redirecting to GitHub for authorization: \n\t', redirectUrl);
 
       return temporaryRedirect(undefined, {
         'Set-Cookie': cookie,
@@ -54,36 +60,39 @@ export class GithubController {
         308: Type.Undefined(),
       },
     },
-    handler: async ({ query }) => {
+    handler: async ({ query, headers }) => {
       const { code, state } = query;
 
       if (!code || !state) return internalServerError({ message: 'Missing code or state' });
-      const cookieState = this.githubService.getState(state);
+      const cookieState = this.githubService.getState(headers?.get('cookie') ?? '');
 
       if (state !== cookieState) return internalServerError({ message: 'Invalid state' });
 
-      const token = await this.githubService.accessToken(code, state);
-      if (!token) return internalServerError({ message: 'Failed to get access token' });
+      const githubToken = await this.githubService.accessToken(code, state);
+      if (!githubToken) return internalServerError({ message: 'Failed to get access token' });
 
-      const user = await this.githubService.userData(token.accessToken);
+      const user = await this.githubService.userData(githubToken.access_token);
       if (!user) return internalServerError({ message: 'Failed to get user data' });
 
       const authUser =
         (await this.githubService.userByProvider(user.id)) ??
         (await this.githubService.create(user));
-
       if (!authUser) return internalServerError({ message: 'Failed to get user' });
 
-      const tokens = await this.githubService.tokens(authUser);
+      const tokens = await this.authService.tokens(authUser);
+      console.log('Tokens: \n\t', tokens);
 
       if (!tokens) return internalServerError({ message: 'Failed to get tokens' });
       const cookieAccessToken = this.githubService.createCookie(tokens.accessToken);
       const cookieRefreshToken = this.githubService.createCookie(tokens.refreshToken);
-      const cookieExpiresIn = this.githubService.createCookie(tokens.expiresIn.toString());
+      // const cookieExpiresIn = this.githubService.createCookie(tokens.expiresIn.toString());
+
+      const setCookie = [cookieAccessToken, cookieRefreshToken].join(',');
+      console.log('Set-Cookie: \n\t', setCookie);
 
       return permanentRedirect(undefined, {
-        Location: 'http://localhost',
-        'Set-Cookie': [cookieAccessToken, cookieRefreshToken, cookieExpiresIn].join(','),
+        Location: 'http://127.0.0.1:5000',
+        'Set-Cookie': setCookie,
       });
     },
   });

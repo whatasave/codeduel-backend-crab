4a49345731911d700df06dc5cf9ac2a0a5f7aa21
Kaj Rusilowski <xedom08@gmail.com>
2025-04-22 20:15:07 +0200

    add jwt and token configuration to auth config

--- packages/api/src/route/auth/config.ts
+++ packages/api/src/route/auth/config.ts
@@ -4,4 +4,16 @@ import { Config as GithubConfig } from './github/config';
 export type Config = Static<typeof Config>;
 export const Config = Type.Object({
   github: GithubConfig,
+  jwt: Type.Object({
+    secret: Type.String(),
+    expiresIn: Type.String(),
+    issuer: Type.String(),
+    audience: Type.String(),
+  }),
+  accessToken: Type.Object({
+    expiresIn: Type.String(),
+  }),
+  refreshToken: Type.Object({
+    expiresIn: Type.String(),
+  }),
 });

10db07f355e7a634d44f85712f0635fc18d6fd30
Kaj Rusilowski <xedom08@gmail.com>
2025-04-25 19:48:29 +0200

    update tests

--- packages/api/src/route/user/service.test.ts
+++ packages/api/src/route/user/service.test.ts
@@ -1,89 +1,88 @@
-import { beforeAll, describe, expect, test } from 'bun:test';
-import type { IUserRepository } from './repository';
+import { beforeAll, describe, expect, spyOn, test } from 'bun:test';
+import { UserRepository } from './repository';
 import type { CreateUser, User } from './data';
 import { UserService } from './service';
-
-class MockUserRepository implements IUserRepository {
-  users: User[] = [
-    { id: 1, username: 'albert', createdAt: '', updatedAt: '' },
-    { id: 2, username: 'berta', createdAt: '', updatedAt: '' },
-    { id: 3, username: 'ceasar', createdAt: '', updatedAt: '' },
-    { id: 4, username: 'dora', createdAt: '', updatedAt: '' },
-  ];
-
-  async all(): Promise<User[]> {
-    return this.users;
-  }
-
-  async byUsername(username: User['username']): Promise<User | undefined> {
-    return this.users.find((u) => u.username == username);
-  }
-
-  async byId(id: User['id']): Promise<User | undefined> {
-    return this.users.find((u) => u.id == id);
-  }
-
-  async create(user: CreateUser): Promise<User> {
-    return { ...user, id: 0, createdAt: '', updatedAt: '' };
-  }
-}
+import type { Database } from '@codeduel-backend-crab/database';
 
 describe('Route.User.Services', () => {
-  let repo: MockUserRepository;
+  let db: Database;
+  let repo: UserRepository;
   let serv: UserService;
+  const fakeUser = {
+    id: 3,
+    username: 'ceasar',
+    name: 'Giuglio Cesare',
+    avatar: 'pic.io/avatar.png',
+    backgroundImage: 'pic.io/cover.png',
+    biography: 'the best',
+    createdAt: '',
+    updatedAt: '',
+  } as User;
+  const fakeUsers = [
+    { id: 1, username: 'albert', createdAt: '', updatedAt: '' },
+    { id: 2, username: 'berta', createdAt: '', updatedAt: '' },
+    fakeUser,
+    { id: 4, username: 'dora', createdAt: '', updatedAt: '' },
+  ] as User[];
 
   beforeAll(() => {
-    repo = new MockUserRepository();
+    db = {} as Database;
+    repo = new UserRepository(db);
     serv = new UserService(repo);
   });
 
-  test('all', async () => {
+  test('should return all the users', async () => {
+    const spyAll = spyOn(repo, 'all').mockResolvedValue(fakeUsers);
     const users = await serv.all();
-    expect(users.length).toBe(repo.users.length);
-    expect(users).toEqual(repo.users);
+
+    expect(spyAll).toHaveBeenCalledWith();
+    expect(spyAll).toHaveBeenCalledTimes(1);
+    expect(users).toEqual(fakeUsers);
   });
 
-  test('byUsername', async () => {
-    const searchingUser = repo.users[2];
-    if (!searchingUser) throw new Error('user not found');
+  test('should return a user with the same username', async () => {
+    const spyByUsername = spyOn(repo, 'byUsername').mockResolvedValue(fakeUser);
+    const user = await serv.byUsername(fakeUser.username);
 
-    const user = await serv.byUsername(searchingUser.username);
-    expect(user).toEqual(searchingUser);
+    expect(spyByUsername).toHaveBeenCalledWith(fakeUser.username);
+    expect(spyByUsername).toHaveBeenCalledTimes(1);
+    expect(user).toEqual(fakeUser);
   });
 
-  test('byId', async () => {
-    const searchingUser = repo.users[2];
-    if (!searchingUser) throw new Error('user not found');
+  test('should return the user with the same id', async () => {
+    const spyById = spyOn(repo, 'byId').mockResolvedValue(fakeUser);
+    const user = await serv.byId(fakeUser.id);
 
-    const user = await serv.byId(searchingUser.id);
-    expect(user).toEqual(searchingUser);
+    expect(spyById).toHaveBeenCalledWith(fakeUser.id);
+    expect(spyById).toHaveBeenCalledTimes(1);
+    expect(user).toEqual(fakeUser);
   });
 
   describe('should create user', () => {
     test('with only required attributes', async () => {
-      const newUser = { username: 'fabio' } satisfies CreateUser;
-
+      const newUser = { username: fakeUser.username } satisfies CreateUser;
+      const spyCreate = spyOn(repo, 'create').mockResolvedValue(fakeUser);
       const savedUser = await serv.create(newUser);
+
+      expect(spyCreate).toHaveBeenCalledWith(newUser);
+      expect(spyCreate).toHaveBeenCalledTimes(1);
+
       expect(savedUser.id).toBeNumber();
       expect(savedUser.username).toEqual(newUser.username);
     });
 
     test('with all attributes', async () => {
       const newUser = {
-        username: 'fabio',
-        name: 'Fabiano Desmond',
-        avatar: 'pic.io/avatar.png',
-        backgroundImage: 'pic.io/profile.png',
-        biography: 'the best',
+        username: fakeUser.username,
+        name: fakeUser.name,
+        avatar: fakeUser.avatar,
+        backgroundImage: fakeUser.backgroundImage,
+        biography: fakeUser.biography,
       } satisfies CreateUser;
 
       const savedUser = await serv.create(newUser);
       expect(savedUser.id).toBeNumber();
-      expect(savedUser.username).toEqual(newUser.username);
-      expect(savedUser.name).toEqual(newUser.name);
-      expect(savedUser.avatar).toEqual(newUser.avatar);
-      expect(savedUser.backgroundImage).toEqual(newUser.backgroundImage);
-      expect(savedUser.biography).toEqual(newUser.biography);
+      expect(savedUser).toEqual(fakeUser);
     });
   });
 });

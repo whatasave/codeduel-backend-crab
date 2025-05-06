9fbb6fa043bcd1fef1db789a908e567b7b5d2a54
Michi Palazzo <palazzomichi@gmail.com>
2025-05-06 18:56:12 +0200

    refactor tests

--- packages/api/src/route/challenge/repository.test.ts
+++ packages/api/src/route/challenge/repository.test.ts
@@ -3,7 +3,7 @@ import { beforeEach, describe, expect, test } from 'bun:test';
 import { setupTestDatabase } from '../../test';
 import { ChallengeRepository } from './repository';
 import { UserRepository } from '../user/repository';
-import type { Challenge } from './data';
+import type { Challenge, CreateChallenge } from './data';
 
 describe('Challenge Repository', () => {
   let db: Database;
@@ -16,202 +16,172 @@ describe('Challenge Repository', () => {
     users = new UserRepository(db);
   });
 
-  test('should create a challenge', async () => {
-    const user = await users.create({
-      username: 'test',
-    });
+  describe('create', () => {
+    let createChallenge: CreateChallenge;
 
-    const challenge = await challenges.create({
-      ownerId: user.id,
-      title: 'Test Challenge',
-      description: 'This is a test challenge',
-      content: 'print("Hello, World!")',
-    });
-
-    expect(challenge).toMatchObject({
-      ownerId: user.id,
-      title: 'Test Challenge',
-      description: 'This is a test challenge',
-      content: 'print("Hello, World!")',
-    });
-
-    if (!challenge) throw new Error();
-
-    await db
-      .selectFrom('challenge')
-      .where('id', '=', challenge.id)
-      .selectAll()
-      .executeTakeFirstOrThrow();
-  });
-
-  test('should return undefined on create conflict', async () => {
-    const user = await users.create({
-      username: 'test',
-    });
-
-    const challenge = await challenges.create({
-      ownerId: user.id,
-      title: 'Test Challenge',
-      description: 'This is a test challenge',
-      content: 'print("Hello, World!")',
-    });
-
-    if (!challenge) throw new Error();
-
-    const duplicateChallenge = await challenges.create({
-      ownerId: user.id,
-      title: 'Test Challenge',
-      description: 'This is a test challenge',
-      content: 'print("Hello, World!")',
-    });
-
-    expect(duplicateChallenge).toBeUndefined();
-  });
-
-  test('should get a challenge by id', async () => {
-    const user = await users.create({
-      username: 'test',
-    });
-
-    const challenge = await challenges.create({
-      ownerId: user.id,
-      title: 'Test Challenge',
-      description: 'This is a test challenge',
-      content: 'print("Hello, World!")',
-    });
-
-    if (!challenge) throw new Error();
-
-    const foundChallenge = await challenges.byId(challenge.id);
-
-    expect(foundChallenge).toMatchObject({
-      id: challenge.id,
-      ownerId: user.id,
-      title: 'Test Challenge',
-      description: 'This is a test challenge',
-      content: 'print("Hello, World!")',
-    });
-  });
-
-  test('should return undefined if challenge not found', async () => {
-    const user = await users.create({
-      username: 'test',
-    });
-
-    const challenge = await challenges.create({
-      ownerId: user.id,
-      title: 'Test Challenge',
-      description: 'This is a test challenge',
-      content: 'print("Hello, World!")',
-    });
-
-    if (!challenge) throw new Error();
-
-    const foundChallenge = await challenges.byId(challenge.id + 1);
-
-    expect(foundChallenge).toBeUndefined();
-  });
-
-  test('should get all challenges', async () => {
-    const user = await users.create({
-      username: 'test',
-    });
-
-    const created = await Promise.all([
-      challenges.create({
+    beforeEach(async () => {
+      const user = await users.create({
+        username: 'test',
+      });
+      createChallenge = {
         ownerId: user.id,
-        title: 'Test Challenge 1',
-        description: 'This is a test challenge 1',
+        title: 'Test Challenge',
+        description: 'This is a test challenge',
         content: 'print("Hello, World!")',
-      }),
-      challenges.create({
+      };
+    });
+
+    test('should create a challenge', async () => {
+      const challenge = await challenges.create(createChallenge);
+
+      expect(challenge).toMatchObject(createChallenge);
+
+      await db
+        .selectFrom('challenge')
+        .where('id', '=', challenge.id)
+        .selectAll()
+        .executeTakeFirstOrThrow();
+    });
+  });
+
+  describe('byId', () => {
+    test('should get a challenge by id', async () => {
+      const user = await users.create({
+        username: 'test',
+      });
+
+      const createChallenge = {
         ownerId: user.id,
-        title: 'Test Challenge 2',
-        description: 'This is a test challenge 2',
+        title: 'Test Challenge',
+        description: 'This is a test challenge',
         content: 'print("Hello, World!")',
-      }),
-    ]);
+      };
 
-    const all = await challenges.all();
+      const challenge = await challenges.create(createChallenge);
 
-    expect(all).toEqual(created as Challenge[]);
-  });
+      const foundChallenge = await challenges.byId(challenge.id);
 
-  test('should update a challenge', async () => {
-    const user = await users.create({
-      username: 'test',
+      expect(foundChallenge).toMatchObject(createChallenge);
     });
 
-    const challenge = await challenges.create({
-      ownerId: user.id,
-      title: 'Test Challenge',
-      description: 'This is a test challenge',
-      content: 'print("Hello, World!")',
-    });
+    test('should return undefined if challenge not found', async () => {
+      const user = await users.create({
+        username: 'test',
+      });
 
-    if (!challenge) throw new Error();
+      const challenge = await challenges.create({
+        ownerId: user.id,
+        title: 'Test Challenge',
+        description: 'This is a test challenge',
+        content: 'print("Hello, World!")',
+      });
 
-    const updatedChallenge = await challenges.update({
-      id: challenge.id,
-      title: 'Updated Challenge',
-      description: 'This is an updated test challenge',
-      content: 'print("Hello, Updated World!")',
-    });
+      const foundChallenge = await challenges.byId(challenge.id + 1);
 
-    expect(updatedChallenge).toMatchObject({
-      id: challenge.id,
-      title: 'Updated Challenge',
-      description: 'This is an updated test challenge',
-      content: 'print("Hello, Updated World!")',
+      expect(foundChallenge).toBeUndefined();
     });
   });
 
-  test('should return undefined on update not found', async () => {
-    const user = await users.create({
-      username: 'test',
+  describe('all', () => {
+    test('should get all challenges', async () => {
+      const user = await users.create({
+        username: 'test',
+      });
+
+      const created = await Promise.all([
+        challenges.create({
+          ownerId: user.id,
+          title: 'Test Challenge 1',
+          description: 'This is a test challenge 1',
+          content: 'print("Hello, World!")',
+        }),
+        challenges.create({
+          ownerId: user.id,
+          title: 'Test Challenge 2',
+          description: 'This is a test challenge 2',
+          content: 'print("Hello, World!")',
+        }),
+      ]);
+
+      const all = await challenges.all();
+
+      expect(all).toEqual(created as Challenge[]);
     });
-
-    const challenge = await challenges.create({
-      ownerId: user.id,
-      title: 'Test Challenge',
-      description: 'This is a test challenge',
-      content: 'print("Hello, World!")',
-    });
-
-    if (!challenge) throw new Error();
-
-    const updatedChallenge = await challenges.update({
-      id: challenge.id + 1,
-      title: 'Updated Challenge',
-      description: 'This is an updated test challenge',
-      content: 'print("Hello, Updated World!")',
-    });
-
-    expect(updatedChallenge).toBeUndefined();
   });
 
-  test('should delete a challenge', async () => {
-    const user = await users.create({
-      username: 'test',
+  describe('update', () => {
+    test('should update a challenge', async () => {
+      const user = await users.create({
+        username: 'test',
+      });
+
+      const challenge = await challenges.create({
+        ownerId: user.id,
+        title: 'Test Challenge',
+        description: 'This is a test challenge',
+        content: 'print("Hello, World!")',
+      });
+
+      const updatedChallenge = await challenges.update({
+        id: challenge.id,
+        title: 'Updated Challenge',
+        description: 'This is an updated test challenge',
+        content: 'print("Hello, Updated World!")',
+      });
+
+      expect(updatedChallenge).toMatchObject({
+        id: challenge.id,
+        title: 'Updated Challenge',
+        description: 'This is an updated test challenge',
+        content: 'print("Hello, Updated World!")',
+      });
     });
 
-    const challenge = await challenges.create({
-      ownerId: user.id,
-      title: 'Test Challenge',
-      description: 'This is a test challenge',
-      content: 'print("Hello, World!")',
+    test('should return undefined on update not found', async () => {
+      const user = await users.create({
+        username: 'test',
+      });
+
+      const challenge = await challenges.create({
+        ownerId: user.id,
+        title: 'Test Challenge',
+        description: 'This is a test challenge',
+        content: 'print("Hello, World!")',
+      });
+
+      const updatedChallenge = await challenges.update({
+        id: challenge.id + 1,
+        title: 'Updated Challenge',
+        description: 'This is an updated test challenge',
+        content: 'print("Hello, Updated World!")',
+      });
+
+      expect(updatedChallenge).toBeUndefined();
     });
+  });
 
-    if (!challenge) throw new Error();
+  describe('delete', () => {
+    test('should delete a challenge', async () => {
+      const user = await users.create({
+        username: 'test',
+      });
 
-    const deleted = await challenges.delete(challenge.id);
+      const challenge = await challenges.create({
+        ownerId: user.id,
+        title: 'Test Challenge',
+        description: 'This is a test challenge',
+        content: 'print("Hello, World!")',
+      });
 
-    expect(deleted).toBe(true);
+      const deleted = await challenges.delete(challenge.id);
 
-    const foundChallenge = await challenges.byId(challenge.id);
-    expect(foundChallenge).toBeUndefined();
+      expect(deleted).toBe(true);
 
-    const all = await challenges.all();
-    expect(all).toEqual([]);
+      const foundChallenge = await challenges.byId(challenge.id);
+      expect(foundChallenge).toBeUndefined();
+
+      const all = await challenges.all();
+      expect(all).toEqual([]);
+    });
   });
 });

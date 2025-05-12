bb0aedca312eeaa0849f42879763c422c573f94a
Michi Palazzo <palazzomichi@gmail.com>
2025-05-13 00:05:20 +0200

    add test cases

--- packages/database/seeds/mock_challenges.ts
+++ packages/database/seeds/mock_challenges.ts
@@ -10,15 +10,14 @@ export async function seed(db: Kysely<DB>): Promise<void> {
     .returningAll()
     .executeTakeFirstOrThrow();
 
-  await db
+  const { id: challenge_id } = await db
     .insertInto('challenge')
-    .values([
-      {
-        owner_id,
-        title: 'Two Sum',
-        description:
-          'Given an array of integers, return indices of the two numbers such that they add up to a specific target.',
-        content: `# Two Sum
+    .values({
+      owner_id,
+      title: 'Two Sum',
+      description:
+        'Given an array of integers, return indices of the two numbers such that they add up to a specific target.',
+      content: `# Two Sum
 Given an array of integers \`nums\` and an integer \`target\`, return indices of the two numbers such that they add up to target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -50,7 +49,38 @@ You can return the answer in any order.
 * \`2 <= nums.length <= 10^4\`
 * \`-10^9 <= nums[i] <= 10^9\`
 * \`-10^9 <= target <= 10^9\``,
+    })
+    .returningAll()
+    .executeTakeFirstOrThrow();
+
+  await db
+    .insertInto('test_case')
+    .values([
+      {
+        challenge_id,
+        input: '9\n4\n2 7 11 15',
+        output: '0 1',
+        hidden: false,
+      },
+      {
+        challenge_id,
+        input: '6\n3\n3 2 4',
+        output: '1 2',
+        hidden: false,
+      },
+      {
+        challenge_id,
+        input: '6\n2\n3 3',
+        output: '0 1',
+        hidden: false,
+      },
+      {
+        challenge_id,
+        input: '0\n2\n0 0',
+        output: '0 1',
+        hidden: true,
       },
     ])
+    .returningAll()
     .execute();
 }

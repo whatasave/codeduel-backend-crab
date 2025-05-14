2a3c983308a9fb46e154deeeaa1a7b0f8bb0fd5a
Kaj Rusilowski <xedom08@gmail.com>
2025-05-15 01:40:43 +0200

    refactor controller tests

--- packages/api/src/route/auth/github/controller.test.ts
+++ packages/api/src/route/auth/github/controller.test.ts
@@ -52,50 +52,50 @@ describe('Route.Auth.Github.Controller', () => {
     expect(routes.sort()).toEqual(expectedRoutes);
   });
 
-  test('should redirect to github login page', async () => {
+  describe('GET /', () => {
     const redirectUrl = 'fe.codeduel.it';
     const state = 'state';
     const stateCookie = `${config.github.stateCookie.name}=${state}`;
     const authorizationUrl = 'github-login-url';
-    const mockHeader = {
-      'user-agent': 'codeduel.it/1.0',
-      'x-forwarded-for': '9.11.69.420',
-    };
+    const mockHeader = { 'user-agent': 'codeduel.it/1.0', 'x-forwarded-for': '9.11.69.420' };
 
-    const spyEncodeState = spyOn(authService, 'encodeState').mockReturnValue(state);
-    const spyAuthorizationUrl = spyOn(service, 'authorizationUrl').mockReturnValue(
-      authorizationUrl
-    );
+    test('should redirect to github login page', async () => {
+      const spyEncodeState = spyOn(authService, 'encodeState').mockReturnValue(state);
+      const spyAuthorizationUrl = spyOn(service, 'authorizationUrl').mockReturnValue(
+        authorizationUrl
+      );
+      const response = await controller.login.handler({
+        method: 'GET',
+        path: '/',
+        query: { redirect: redirectUrl },
+        params: {},
+        body: undefined,
+        headers: new Headers(mockHeader),
+      });
 
-    const response = await controller.login.handler({
-      method: 'GET',
-      path: '/',
-      query: { redirect: redirectUrl },
-      params: {},
-      body: undefined,
-      headers: new Headers(mockHeader),
+      expect(spyEncodeState).toHaveBeenCalledWith({
+        csrfToken: expect.any(String) as unknown as string,
+        redirect: redirectUrl,
+        ip: mockHeader['x-forwarded-for'],
+        userAgent: mockHeader['user-agent'],
+      });
+      expect(spyEncodeState).toHaveBeenCalledTimes(1);
+
+      expect(spyAuthorizationUrl).toHaveBeenCalledWith(state);
+      expect(spyAuthorizationUrl).toHaveBeenCalledTimes(1);
+
+      expect(response.status).toEqual(307);
+      if (!response.headers) throw new Error('Response headers are undefined');
+      expect(response.headers.get('location')).toEqual(authorizationUrl);
+      expect(response.headers.get('content-type')).toEqual('text/plain');
+      expect(response.headers.get('set-cookie')).toEqual(stateCookie);
+      expect(response.body).toEqual(`Redirecting to ${authorizationUrl}`);
     });
 
-    expect(spyEncodeState).toHaveBeenCalledWith({
-      csrfToken: expect.any(String) as unknown as string,
-      redirect: redirectUrl,
-      ip: mockHeader['x-forwarded-for'],
-      userAgent: mockHeader['user-agent'],
-    });
-    expect(spyEncodeState).toHaveBeenCalledTimes(1);
-
-    expect(spyAuthorizationUrl).toHaveBeenCalledWith(state);
-    expect(spyAuthorizationUrl).toHaveBeenCalledTimes(1);
-
-    expect(response.status).toEqual(307);
-    if (!response.headers) throw new Error('Response headers are undefined');
-    expect(response.headers.get('location')).toEqual(authorizationUrl);
-    expect(response.headers.get('content-type')).toEqual('text/plain');
-    expect(response.headers.get('set-cookie')).toEqual(stateCookie);
-    expect(response.body).toEqual(`Redirecting to ${authorizationUrl}`);
+    test('should redirect to github login page with default user agent', async () => {});
   });
 
-  test('should set the refresh and access token', async () => {
+  describe('GET /callback', () => {
     const mockDate = new Date('2025-05-12T18:39:26.183Z').toString();
     const mockState = {
       redirect: 'redirect',
@@ -132,57 +132,60 @@ describe('Route.Auth.Github.Controller', () => {
     const mockHeaders = {
       cookie: `${config.github.stateCookie.name}=${mockStateString}`,
     };
+    test('should set the refresh and access token', async () => {
+      const spyDecodeState = spyOn(authService, 'decodeState').mockReturnValue(mockState);
+      const spyExchange = spyOn(service, 'exchangeCodeForToken').mockResolvedValue(mockToken);
+      const spyUserData = spyOn(service, 'userData').mockResolvedValue(mockGithubUser);
+      const spyCreate = spyOn(service, 'create').mockResolvedValue([mockAuth, mockUser]);
+      const spyAccessToken = spyOn(authService, 'accessToken').mockResolvedValue(mockAccessToken);
+      const spyRefreshToken = spyOn(authService, 'refreshToken').mockResolvedValue(
+        mockRefreshToken
+      );
+      const spyCreateSession = spyOn(service, 'createSession').mockResolvedValue();
 
-    const spyDecodeState = spyOn(authService, 'decodeState').mockReturnValue(mockState);
-    const spyExchange = spyOn(service, 'exchangeCodeForToken').mockResolvedValue(mockToken);
-    const spyUserData = spyOn(service, 'userData').mockResolvedValue(mockGithubUser);
-    const spyCreate = spyOn(service, 'create').mockResolvedValue([mockAuth, mockUser]);
-    const spyAccessToken = spyOn(authService, 'accessToken').mockResolvedValue(mockAccessToken);
-    const spyRefreshToken = spyOn(authService, 'refreshToken').mockResolvedValue(mockRefreshToken);
-    const spyCreateSession = spyOn(authService, 'createSession').mockResolvedValue();
+      const response = await controller.callback.handler({
+        method: 'GET',
+        path: '/callback',
+        query: { code: mockCode, state: mockStateString },
+        params: {},
+        body: undefined,
+        headers: new Headers(mockHeaders),
+      });
 
-    const response = await controller.callback.handler({
-      method: 'GET',
-      path: '/callback',
-      query: { code: mockCode, state: mockStateString },
-      params: {},
-      body: undefined,
-      headers: new Headers(mockHeaders),
+      expect(spyDecodeState).toHaveBeenCalledWith(mockStateString);
+      expect(spyDecodeState).toHaveBeenCalledTimes(1);
+
+      expect(spyExchange).toHaveBeenCalledWith(mockCode, mockStateString);
+      expect(spyExchange).toHaveBeenCalledTimes(1);
+
+      expect(spyUserData).toHaveBeenCalledWith(mockToken.access_token);
+      expect(spyUserData).toHaveBeenCalledTimes(1);
+
+      expect(spyCreate).toHaveBeenCalledWith(mockGithubUser);
+      expect(spyCreate).toHaveBeenCalledTimes(1);
+
+      expect(spyAccessToken).toHaveBeenCalledWith(mockUser);
+      expect(spyAccessToken).toHaveBeenCalledTimes(1);
+
+      expect(spyRefreshToken).toHaveBeenCalledWith(mockUser, expect.any(String));
+      expect(spyRefreshToken).toHaveBeenCalledTimes(1);
+
+      expect(spyCreateSession).toHaveBeenCalledWith(
+        mockUser.id,
+        expect.any(String),
+        mockState.ip,
+        mockState.userAgent
+      );
+      expect(spyCreateSession).toHaveBeenCalledTimes(1);
+
+      expect(response.status).toEqual(307);
+      if (!response.headers) throw new Error('Response headers are undefined');
+      expect(response.headers.get('location')).toEqual(mockState.redirect);
+      expect(response.headers.get('content-type')).toEqual('text/plain');
+      expect(response.headers.get('set-cookie')).toEqual(
+        `${config.accessToken.cookie.name}=${mockAccessToken}, ${config.refreshToken.cookie.name}=${mockRefreshToken}`
+      );
+      expect(response.body).toEqual(`Redirecting to ${mockState.redirect}`);
     });
-
-    expect(spyDecodeState).toHaveBeenCalledWith(mockStateString);
-    expect(spyDecodeState).toHaveBeenCalledTimes(1);
-
-    expect(spyExchange).toHaveBeenCalledWith(mockCode, mockStateString);
-    expect(spyExchange).toHaveBeenCalledTimes(1);
-
-    expect(spyUserData).toHaveBeenCalledWith(mockToken.access_token);
-    expect(spyUserData).toHaveBeenCalledTimes(1);
-
-    expect(spyCreate).toHaveBeenCalledWith(mockGithubUser);
-    expect(spyCreate).toHaveBeenCalledTimes(1);
-
-    expect(spyAccessToken).toHaveBeenCalledWith(mockUser);
-    expect(spyAccessToken).toHaveBeenCalledTimes(1);
-
-    expect(spyRefreshToken).toHaveBeenCalledWith(mockUser, expect.any(String));
-    expect(spyRefreshToken).toHaveBeenCalledTimes(1);
-
-    expect(spyCreateSession).toHaveBeenCalledWith(
-      mockUser.id,
-      expect.any(String),
-      mockState.ip,
-      mockState.userAgent
-    );
-    expect(spyCreateSession).toHaveBeenCalledTimes(1);
-
-    expect(response.status).toEqual(307);
-    if (!response.headers) throw new Error('Response headers are undefined');
-    expect(response.headers.get('location')).toEqual(mockState.redirect);
-    expect(response.headers.get('content-type')).toEqual('text/plain');
-    expect(response.headers.get('set-cookie')).toEqual(
-      `${config.accessToken.cookie.name}=${mockAccessToken}, ${config.refreshToken.cookie.name}=${mockRefreshToken}`
-    );
-    expect(response.body).toEqual(`Redirecting to ${mockState.redirect}`);
   });
 });

141804bb779d8244f43833d3319f8d839d4fa87c
Michi Palazzo <palazzomichi@gmail.com>
2025-05-25 20:39:27 +0300

    shorten html

--- packages/api/src/route/scalar/controller.ts
+++ packages/api/src/route/scalar/controller.ts
@@ -18,18 +18,11 @@ export class ScalarController {
   <head>
     <title>Scalar API Reference</title>
     <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
   </head>
-
   <body>
     <div id="app"></div>
-
-    <!-- Load the Script -->
     <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
-
-    <!-- Initialize the Scalar API Reference -->
     <script>
       Scalar.createApiReference('#app', {
         url: '/openapi',
